8a1ae045bc003f49aa17e5cd9bc3d033f9e965be
Braco <isovicramiz07@gmail.com>
2023-06-30 04:42:05 -0400

    added long sleeve category with 3 selections

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,14 +1,9 @@
 import hats from "./hats";
 import jerseys from "./jerseys";
 import womens from "./womens";
+import shorts from "./shorts";
+import longSleeve from "./longSleeve";
 
-const categories = [
-  // hats: hats,
-  // jerseys: jerseys,
-  // womens: womens,
-  hats,
-  jerseys,
-  womens,
-];
+const categories = [hats, jerseys, longSleeve, womens, shorts];
 
 export default categories;
---
+++ src/data/longSleeve.js
@@ -0,0 +1,42 @@
+import mashupCrewneckOne from "../images/products/heat-white-mashup-crewneck-1.jpg";
+import mashupCrewneckTwo from "../images/products/heat-white-mashup-crewneck-2.jpg";
+import satinJacketOne from "../images/products/heat-black-satin-jacket-1.jpg";
+import satinJacketTwo from "../images/products/heat-black-satin-jacket-2.jpg";
+import satinJacketThree from "../images/products/heat-black-satin-jacket-3.jpg";
+import bomberJacketOne from "../images/products/heat-white-bomber-jacket-1.jpg";
+import bomberJacketTwo from "../images/products/heat-white-bomber-jacket-2.jpg";
+import bomberJacketThree from "../images/products/heat-white-bomber-jacket-3.jpg";
+import whiteCrewneck from "../images/products/heat-white-crewneck.jpg";
+
+const longSleeve = [
+  {
+    name: "Mashup Crewneck",
+    id: "mashup-crewneck",
+    category: "long sleeve",
+    price: "$80",
+    images: [mashupCrewneckOne, mashupCrewneckTwo],
+  },
+  {
+    name: "White Crewneck",
+    id: "white-crewneck",
+    category: "long sleeve",
+    price: "$90",
+    images: [whiteCrewneck],
+  },
+  {
+    name: "Satin Jacket",
+    id: "satin-jacket",
+    category: "long sleeve",
+    price: "$140",
+    images: [satinJacketOne, satinJacketTwo, satinJacketThree],
+  },
+  {
+    name: "Bomber Jacket",
+    id: "bomber-jacket",
+    category: "long sleeve",
+    price: "$200",
+    images: [bomberJacketOne, bomberJacketTwo, bomberJacketThree],
+  },
+];
+
+export default longSleeve;
