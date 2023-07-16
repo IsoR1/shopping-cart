ea36353171ae6d6e69bfb6817c25c4c44767c761
Braco <isovicramiz07@gmail.com>
2023-07-15 21:41:34 -0400

    Add component to increment and decremnt quantity of item

--- src/components/CartCountControl.js
+++ src/components/CartCountControl.js
@@ -3,11 +3,6 @@ import React from "react";
 const CartCountControl = ({ item, count, removeItemFromCart }) => {
   return (
     <div className="cart-count-control-div">
-      {/* <span>-</span>
-      <br />
-      <span>{count}</span>
-      <br />
-      <span>+</span> */}
       <span onClick={() => removeItemFromCart(item)}>-</span>
       <span>{count}</span>
       <span>+</span>
